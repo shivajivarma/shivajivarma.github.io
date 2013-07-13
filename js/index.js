072f7e74167093df4eb83fa98ef6842f34f3d938
Shivaji Varma <postman@shivajivarma.com>
2013-07-13 23:07:51 +0530

    :)

---
+++ js/index.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function ($) {
+
+		
+				
+		$('#decorative-img').mouseenter(function(){	
+			$('#decorative-img').attr("src","images/decorative-2.png").delay(4000);
+		}).mouseleave(function(){	
+			$('#decorative-img').attr("src","images/decorative.png").delay(4000);
+		});
+
+		        
+});
