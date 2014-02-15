7dae4df2d39308d6c74f1b3970187f8f52a110a5
Shivaji Varma <contact@shivajivarma.com>
2014-02-16 00:13:05 +0530

    :)

--- assets/javascripts/about.js
+++ assets/javascripts/about.js
@@ -4,8 +4,7 @@ function contactForm(){
 		url: 'http://php-shivajivarma.rhcloud.com/mail.php',
 		data: {name:$('#name').val(),sender:$('#sender').val(),message:$('#message').val(),subject:"Contact Form"},
 		success: function(status){
-		alert(status);
-		 if(status == 'Success')
+		 if(status == 'success')
 			$('#myModal .modal-body').html('Message send successfully :)');
 		else 
 			$('#myModal .modal-body').html('Unable to send. Please try after sometime.');
