e866fc8ee6379a35b0f2eabed0843f6940fd3591
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2016-01-02 01:26:32 +0530

    Jekyll 3.0.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,43 @@
-var $root=$("html, body");$("a").click(function(){var a=$.attr(this,"href");var b;if(a=="#"){b=$("body");}else{if(a.match(/^#[a-zA-Z0-9]+$/g)){b=$("#"+a.substring(1));}else{return true;}}$root.animate({scrollTop:$(b).offset().top},500,function(){window.location.hash=a;});return false;});$(document).ready(function(){$.material.init();$(".dropdown-toggle").dropdown();$("#owl-index").owlCarousel({autoPlay:7000,stopOnHover:true,paginationSpeed:1000,singleItem:true,autoHeight:false});var a=parseInt($(".owl-wrapper").css("height"),10);$(".owl-item").each(function(){$(this).css("padding-top",(a-parseInt($(this).css("height"),10))/2+"px");});});$(document).on("ready page:load",function(){Prism.highlightAll();});
+var $root = $('html, body');
+
+$('a').click(function() {
+  var href = $.attr(this, 'href');
+  var hash;
+    
+  if(href == '#'){
+    hash = $('body');
+  }else if(href.match(/^#[a-zA-Z0-9]+$/g)){
+    hash = $('#'+href.substring(1));
+  }else 
+	return true;
+	
+  $root.animate({
+      scrollTop: $(hash).offset().top
+    }, 500, function () {
+      window.location.hash = href;
+  });
+ 
+  return false;
+});
+
+$(document).ready(function() {
+	$.material.init();
+	$('.dropdown-toggle').dropdown();
+
+	$("#owl-index").owlCarousel({  
+		autoPlay : 7000,
+		stopOnHover : true,
+		paginationSpeed : 1000,
+		singleItem : true,
+		autoHeight : false,
+     });
+	 
+	 var owlWrapperHeight = parseInt($('.owl-wrapper').css('height'),10);
+	 $('.owl-item').each(function(){
+		$(this).css('padding-top',(owlWrapperHeight-parseInt($(this).css('height'), 10))/2 + 'px');
+	 });
+});
+
+$(document).on('ready page:load', function() {
+    Prism.highlightAll();
+});
