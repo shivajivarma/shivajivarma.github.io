6f55623b116006a453a01cfa7253fd30c772f1a7
shivajivarma <contact@shivajivarma.com>
2017-06-04 11:23:36 +0530

    removed unwanted js

--- assets/js/sum-of-two-numbers.js
+++ assets/js/sum-of-two-numbers.js
@@ -1,1 +1,19 @@
-$(document).ready(function(){var d=getUrlParameter("a");var c=getUrlParameter("b");if(d!=""&&c!=""){if($.isNumeric(d)&&$.isNumeric(c)){$("#output").html(+d+ +c);}else{$("#output").html("Enter only numbers.");}}else{$("#output").html("Please fill the input and hit add button.");}});function getUrlParameter(b){b=b.replace(/[\[]/,"\\[").replace(/[\]]/,"\\]");var d="[\\?&]"+b+"=([^&#]*)";var c=new RegExp(d);var a=c.exec(window.location.href);if(a==null){return"";}else{return unescape(a[1]).replace(/<script.*>.*<\/script>/g,"");}}
+$(document).ready(function(){
+	var a = getUrlParameter("a");
+	var b = getUrlParameter("b");
+	if(a!="" && b!=""){
+		if($.isNumeric(a) && $.isNumeric(b)){
+			$("#output").html(+a + +b);
+		}
+		else  $("#output").html("Enter only numbers.");
+	}
+	else $("#output").html("Please fill the input and hit add button.");
+});
+function getUrlParameter( key ){  
+		key = key.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");  
+		var regexExpression = "[\\?&]"+key+"=([^&#]*)";  
+		var regex = new RegExp( regexExpression );  
+		var results = regex.exec( window.location.href );  
+		if( results == null )    return "";  
+			else    return unescape(results[1]).replace(/<script.*>.*<\/script>/g, "");
+}
