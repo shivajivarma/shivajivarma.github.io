32022a406ce138c471cabd5539e3f1f400539161
shivajivarma <contact@shivajivarma.com>
2018-05-04 23:36:14 +0530

    removed owl carosual

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,20 +3,20 @@ var $root = $('html, body');
 $('a').click(function() {
   var href = $.attr(this, 'href');
   var hash;
-    
+
   if(href == '#'){
     hash = $('body');
   }else if(href.match(/^#[a-zA-Z0-9]+$/g)){
     hash = $('#'+href.substring(1));
-  }else 
+  }else
 	return true;
-	
+
   $root.animate({
       scrollTop: $(hash).offset().top
     }, 500, function () {
       window.location.hash = href;
   });
- 
+
   return false;
 });
 
@@ -24,36 +24,21 @@ $(document).ready(function() {
 	$(document).ready(function() { $('body').bootstrapMaterialDesign(); });
 	//$('.dropdown-toggle').dropdown();
 
-	$("#owl-index").owlCarousel({  
-		autoPlay : 7000,
-		stopOnHover : true,
-		paginationSpeed : 1000,
-		singleItem : true,
-		autoHeight : false,
-        pagination: false
-     });
-	 
-	 var owlWrapperHeight = parseInt($('.owl-wrapper').css('height'),10);
-	 $('.owl-item').each(function(){
-		$(this).css('padding-top',(owlWrapperHeight-parseInt($(this).css('height'), 10))/2 + 'px');
-	 });
-	 
-
     // This will take care of asynchronous Google ads
-    setTimeout(function() { 
-      
+    setTimeout(function() {
+
       // We are targeting the first banner ad of AdSense
       var ad = $(".adsbygoogle");
-      
+
       // If the ad contains no innerHTML, ad blockers are at work
       if (ad && ad.html() && ad.html().replace(/\s/g, "").length == 0) {
-		  
-		  $('#adblocker-message').show();		  
-      
+
+		  $('#adblocker-message').show();
+
       }
-      
-    }, 2000); // The ad blocker check is performed 2 seconds after the page load 
-  
+
+    }, 2000); // The ad blocker check is performed 2 seconds after the page load
+
 });
 
 $(document).on('ready page:load', function() {
